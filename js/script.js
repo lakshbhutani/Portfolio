2dd04d366151fa1ba3ef4b1e38387d0c47560e0e
lakshbhutani <33488734+lakshbhutani@users.noreply.github.com>
2017-12-12 23:17:08 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$('.button-hire').click(function(){
+		$('html,body').animate({scrollTop:$('.contacts').offset().top},1000);
+});
+$('.learn-more ,.ion-chevron-down').click(function(){
+		$('html,body').animate({scrollTop:$('.expertise').offset().top+48},1000);
+});
